13a2ef658a20d3a9ee30e9bebdb31e4d1225aa8f
Vlad Adaskalitsa <Moonspellsablle@MacBook-Air-Vlad.local>
2019-07-28 21:29:54 +0300

    Updated 'if' statement

--- hello.sh
+++ hello.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 yourNameIs=$1
 
-if [ "$yourNameIs" == "" ]; then
+if [ -z "$yourNameIs" ]; then
     echo "Hello, friend. Try to enter your name, after hello.sh [Name]"
 else
     echo "Hello, " $yourNameIs
